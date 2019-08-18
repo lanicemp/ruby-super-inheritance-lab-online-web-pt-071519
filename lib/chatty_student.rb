9aed66af6181908533af99d823fd2422c1f11451
LaNice Powell <lanicemp@aol.com>
2019-08-18 16:46:00 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,7 +4,7 @@ class ChattyStudent < Student
     puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
-  def raise_hand(phrase) 
+  def raise_hand 
    9.times do 
      puts phrase
    end 
